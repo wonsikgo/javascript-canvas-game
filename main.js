768a080430ec2d76d2bfec05c31765789d840aad
wonsickgo <45756634+wonsikgo@users.noreply.github.com>
2022-04-15 16:32:39 +0900

    게임 옵션 변경

--- main.js
+++ main.js
@@ -69,7 +69,7 @@ class Enemy {
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
-const player = new Player(x, y, 30, "blue");
+const player = new Player(x, y, 10, "white");
 const projectiles = [];
 const enemies = [];
 
@@ -87,7 +87,8 @@ function spawnEnemies() {
       x = Math.random() * canvas.width;
       y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
     }
-    const color = "green";
+    const color = `hsl(${Math.random() * 360}, 50%, 50%)`;
+
     const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
     const velocity = {
       x: Math.cos(angle),
@@ -100,11 +101,25 @@ function spawnEnemies() {
 let animationId;
 
 function animate() {
-  let animationId = requestAnimationFrame(animate);
-  c.clearRect(0, 0, canvas.width, canvas.height);
+  animationId = requestAnimationFrame(animate);
+  c.fillStyle = "rgba(0, 0, 0, 0.1)";
+  c.fillRect(0, 0, canvas.width, canvas.height);
   player.draw();
-  projectiles.forEach((projectile) => {
+  projectiles.forEach((projectile, index) => {
     projectile.update();
+
+    // 화면의 끝에 도착하면 제거
+    if (
+      projectile.x + projectile.radius < 0 ||
+      projectile.x - projectile.radius > canvas.width ||
+      projectile.y + projectile.radius < 0 ||
+      projectile.y - projectile.radius > canvas.height
+    ) {
+      setTimeout(() => {
+        console.log("화면 끝 도착 제거");
+        projectiles.splice(index, 1);
+      }, 0);
+    }
   });
   enemies.forEach((enemy, index) => {
     enemy.update();
@@ -137,11 +152,11 @@ addEventListener("click", (e) => {
     e.clientX - canvas.width / 2
   );
   const velocity = {
-    x: Math.cos(angle),
-    y: Math.sin(angle),
+    x: Math.cos(angle) * 4,
+    y: Math.sin(angle) * 4,
   };
   projectiles.push(
-    new Projectile(canvas.width / 2, canvas.height / 2, 5, "red", velocity)
+    new Projectile(canvas.width / 2, canvas.height / 2, 5, "wwhite", velocity)
   );
 });
 
