c3d3399c810d9fa03d9b465a86e54fe5c93a689c
wonsickgo <45756634+wonsikgo@users.noreply.github.com>
2022-04-15 14:28:40 +0900

    발사체 애니메이션 적용

--- main.js
+++ main.js
@@ -35,15 +35,41 @@ class Projectile {
     c.fillStyle = this.color;
     c.fill();
   }
+
+  update() {
+    this.draw();
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
 }
 
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const player = new Player(x, y, 30, "blue");
-player.draw();
+const projectiles = [];
 
-window.addEventListener("click", (e) => {
-  const projectile = new Projectile(e.clientX, e.clientY, 5, "red", null);
-  projectile.draw();
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  player.draw();
+  projectiles.forEach((projectile) => {
+    projectile.update();
+  });
+}
+
+addEventListener("click", (e) => {
+  const angle = Math.atan2(
+    e.clientY - canvas.height / 2,
+    e.clientX - canvas.width / 2
+  );
+  const velocity = {
+    x: Math.cos(angle),
+    y: Math.sin(angle),
+  };
+  projectiles.push(
+    new Projectile(canvas.width / 2, canvas.height / 2, 5, "red", velocity)
+  );
 });
+
+animate();
